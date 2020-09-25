764ded40ec56face2c6b0369ac85aa2f49eb6d53
Moritz <moritz.zielke@gmail.com>
2020-09-25 18:30:05 +0200

    src/components/BaseButtonOutline: add it

---
+++ src/components/BaseButtonOutline.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    class="base-button-outline"
+    :disabled="disabled"
+    type="button"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@use "button";
+
+.base-button-outline {
+  @include button.basics;
+
+  color: inherit;
+  background-color: transparent;
+
+  &:active,
+  &:hover,
+  &:focus {
+    background-color: transparent;
+  }
+}
+</style>
